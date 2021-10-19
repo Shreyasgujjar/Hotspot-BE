7221b081b5bf3b507751eb7dfe2e1520cfe368f8
sshivaji <sshivaji@cs.odu.edu>
2021-10-19 09:20:18 -0400

    changes

--- rebootConfig.py
+++ rebootConfig.py
@@ -33,7 +33,7 @@ def resetConfig():
     for data in connectedDevices:
         if data != '':
             os.system("iptables -w -t filter -D vpnhotspot_acl -o " + wlan_details + " -d " + data.split(" ")[3].split("/")[0] + " -j ACCEPT")
-            os.system("iptables -w -t filter -D vpnhotspot_acl -i " + wlan_details + " -d " + data.split(" ")[3].split("/")[0] + " -j ACCEPT")
+            os.system("iptables -w -t filter -D vpnhotspot_acl -i " + wlan_details + " -s " + data.split(" ")[3].split("/")[0] + " -j ACCEPT")
             print(data.split(" ")[3].split("/")[0])
     os.system("iptables -w -t nat -D PREROUTING -i " + wlan_details +" -p udp -d " + dest_ip + " --dport 53 -j DNAT --to-destination 198.224.137.129")
     os.system("iptables -w -t nat -D PREROUTING -i " + wlan_details +" -p tcp -d " + dest_ip + " --dport 53 -j DNAT --to-destination 198.224.137.129")
@@ -41,7 +41,7 @@ def resetConfig():
     os.system("iptables -w -t nat -D vpnhotspot_masquerade -s " + dest_ip + "/24 -o " + lookUpName + " -j MASQUERADE")
     os.system("/system/bin/ip rule del  iif " + wlan_details + " lookup " + str(lookUpNumber) + " priority 17900")
     os.system("/system/bin/ip rule del  iif " + wlan_details + " unreachable priority 17980")
-    os.system("cleiptables -w -t nat -D POSTROUTING -j vpnhotspot_masquerade")
+    os.system("iptables -w -t nat -D POSTROUTING -j vpnhotspot_masquerade")
     os.system("iptables -w -t filter -D vpnhotspot_fwd -i " + wlan_details + " ! -o " + wlan_details + " -j REJECT")
     os.system("iptables -w -t filter -D vpnhotspot_fwd -o " + wlan_details + " -m state --state ESTABLISHED,RELATED -j vpnhotspot_acl")
     os.system("iptables -w -t filter -D vpnhotspot_fwd -i " + wlan_details + " -j vpnhotspot_acl")
