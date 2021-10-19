9caa06948a9d55f765eabb941ea262b056e6c8a1
sshivaji <sshivaji@cs.odu.edu>
2021-10-19 09:04:44 -0400

    changes

--- rebootConfig.py
+++ rebootConfig.py
@@ -26,7 +26,7 @@ def resetConfig():
     os.system("iptables -w -t nat -D PREROUTING -i " + wlan_details +" -p tcp -d " + dest_ip + " --dport 53 -j DNAT --to-destination 198.224.137.129")
     os.system("/system/bin/ip rule del to 198.224.137.129 iif " + wlan_details + " lookup " + str(lookUpNumber) + " priority 17700")
     os.system("iptables -w -t nat -D vpnhotspot_masquerade -s " + dest_ip + "/24 -o " + lookUpName + " -j MASQUERADE")
-    os,system("/system/bin/ip rule del  iif " + wlan_details + " lookup " + lookUpNumber + " priority 17900")
+    os.system("/system/bin/ip rule del  iif " + wlan_details + " lookup " + lookUpNumber + " priority 17900")
     os.system("/system/bin/ip rule del  iif " + wlan_details + " unreachable priority 17980")
     os.system("cleiptables -w -t nat -D POSTROUTING -j vpnhotspot_masquerade")
     os.system("iptables -w -t filter -D vpnhotspot_fwd -i " + wlan_details + " ! -o " + wlan_details + " -j REJECT")
